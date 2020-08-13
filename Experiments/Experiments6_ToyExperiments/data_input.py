aed58f98652f3888b521924dd808a2bf50ffa32d
QuYue <quyue1541@mail.dlut.edu.cn>
2020-08-13 14:16:45 +0800

    11

---
+++ Experiments/Experiments6_ToyExperiments/data_input.py
@@ -0,0 +1,191 @@
+# -*- encoding: utf-8 -*-
+'''
+@Time        :2020/06/26 17:29:26
+@Author      :Qu Yue
+@File        :data_input.py
+@Software    :Visual Studio Code
+Introduction: Input the data.
+'''
+#%% Import Packages
+import torchvision
+import torch.utils.data as Data
+from numpy.random import permutation
+from sklearn.model_selection import train_test_split
+# %matplotlib auto
+#%% Functions
+def data_input(Parm):
+    """Input the data.
+    Input: 
+        dataset_ID: Data set number [1, 2, 3]
+        download: If download the data set [True, False]
+    Output:
+        datasets
+    """
+    # Parameter
+    dataset_ID = Parm.dataset_ID
+    download = Parm.data.MNIST.download
+    # Input Data
+    if dataset_ID == 1: # Disjoint MNIST
+        print('Loading dataset 1: Disjoint MNIST...')
+        data, target = MNIST_input(download)
+        datasets = task_split([data, target], Parm)
+    elif dataset_ID == 2: # Split MNIST
+        print('Loading dataset 2: Split MNIST...')
+        data, target = MNIST_input(download)
+        datasets = task_split([data, target], Parm)
+    elif dataset_ID == 3: # Permuted MNIST
+        print('Loading dataset 3: Permuted MNIST')
+        data, target = MNIST_input(download)
+        datasets = task_split([data, target], Parm)
+    else:
+        print('Please input right dataset number.')
+        return None
+    print('Data is ready.')
+    return datasets
+
+def MNIST_input(download=True):
+    """Input MNIST data.
+    Input:
+        download: If download the data set [True, False]
+    Output:
+        mnist_data
+        mnist_target
+    """
+    def data_combine(data_train, data_test):
+        data = []; target = []
+        for d,t in data_train:
+            data.append(d), target.append(t)
+        for d,t in data_test:
+            data.append(d), target.append(t)
+        return data, target
+    # Data MNIST
+    # trans = torchvision.transforms.Compose([torchvision.transforms.ToTensor(),
+    #                                         torchvision.transforms.Normalize((0.5,), (1.0,))])
+    trans = torchvision.transforms.Compose([torchvision.transforms.ToTensor()])
+    data_train = torchvision.datasets.MNIST(root="../../Data/MNIST",
+                                            train=True,
+                                            transform=trans,
+                                            download=download)
+    data_test = torchvision.datasets.MNIST(root="../../Data/MNIST",
+                                           train=False,
+                                           transform=trans,
+                                           download=download)
+    # Data Combine
+    mnist_data, mnist_target = data_combine(data_train, data_test)
+    return mnist_data, mnist_target
+
+def task_split(dataset, Parm):
+    dataset_ID = Parm.dataset_ID
+    # Task Split
+    datasets = []
+    if dataset_ID == 1: # Disjoint MNIST
+        datasets = [{'data':[], 'target':[]} for i in range(2)]
+        for i in range(len(dataset[1])):
+            if dataset[1][i] < 5:
+                datasets[0]['data'].append(dataset[0][i])
+                datasets[0]['target'].append(dataset[1][i])
+            else: 
+                datasets[1]['data'].append(dataset[0][i])
+                datasets[1]['target'].append(dataset[1][i])
+    elif dataset_ID == 2: # Split MNIST
+        datasets = [{'data':[], 'target':[]} for i in range(5)]
+        for i in range(len(dataset[1])):
+            if dataset[1][i] <= 1:
+                datasets[0]['data'].append(dataset[0][i])
+                datasets[0]['target'].append(dataset[1][i])
+            elif 1 < dataset[1][i] <= 3:
+                datasets[1]['data'].append(dataset[0][i])
+                datasets[1]['target'].append(dataset[1][i])
+            elif 3 < dataset[1][i] <= 5:
+                datasets[2]['data'].append(dataset[0][i])
+                datasets[2]['target'].append(dataset[1][i])
+            elif 5 < dataset[1][i] <= 7:
+                datasets[3]['data'].append(dataset[0][i])
+                datasets[3]['target'].append(dataset[1][i])
+            elif 7 < dataset[1][i] <= 9:
+                datasets[4]['data'].append(dataset[0][i])
+                datasets[4]['target'].append(dataset[1][i])
+    elif dataset_ID == 3: # Permuted MNIST
+        tasks_ID = Parm.data.tasks['Permuted MNIST']
+        permute_index = [list(range(len(dataset[0][0].view(-1))))]
+        for i in range(tasks_ID-1):
+            permute_index.append(permutation(permute_index[0]))
+        datasets = [{'data':[], 'target':[], 'index': permute_index[i]} for i in range(tasks_ID)]
+        for i in range(len(dataset[1])):
+            d = dataset[0][i].view(-1)
+            for j in range(tasks_ID):
+                temp_d = d[datasets[j]['index']]
+                temp_d = temp_d.view(dataset[0][0].shape)
+                datasets[j]['data'].append(temp_d)
+                datasets[j]['target'].append(dataset[1][i])
+    else:
+        print('Please input right dataset number.')
+        return None
+    return datasets
+
+def data_split(data, target, test_size=0.2, random_state=0):
+    """Data split to train and test.
+    Input:
+        data: data need to be split
+        target: target need to be split
+        test_size: ratio of test set to total data set (default=0.2)
+        random_state: random seed (default=0)
+    Output::
+        splitted_data: [[X_train, y_train], [X_test, y_test]]
+    """
+    X_train,X_test, y_train, y_test = train_test_split(data, target, test_size=test_size, random_state=random_state)
+    return [[X_train, y_train], [X_test, y_test]]
+
+
+#%% Class
+class DATA():
+    def __init__(self):
+        self.download = True
+        self.path = None
+
+class DATASET():
+    def __init__(self):
+        self.MNIST = DATA()
+        self.MNIST.path='../../Data/MNIST'
+        self.data_dict = {1:'Disjoint MNIST', 2:'Split MNIST', 3:'Permuted MNIST'}
+        self.tasks = {'Disjoint MNIST':2, 'Split MNIST':5, 'Permuted MNIST': 3}
+if __name__ == "__main__":
+    class PARM():
+        def __init__(self):
+            self.data = DATASET()
+            self.dataset_ID = 1
+        @property
+        def dataset_name(self):
+            return self.data.data_dict[self.dataset_ID]  
+        @property
+        def task_number(self):
+            return self.data.tasks[self.dataset_name]            
+
+    Parm = PARM()
+    Parm.data.MNIST.download = False
+    datasets = data_input(Parm)
+# #%% dataset 3
+#     Parm.dataset_ID = 3
+#     datasets = data_input(Parm)
+#     import numpy as np
+#     import matplotlib.pyplot as plt
+#     n = 2
+#     plt.subplot(1,3,1)
+#     plt.imshow(datasets[0]['data'][n][0])
+#     plt.subplot(2,3,2)
+#     d = datasets[1]['data'][n][0]
+#     plt.imshow(d)
+#     plt.subplot(2,3,5)
+#     dd = d.view(-1)
+#     inverse_index = np.argsort(datasets[1]['index'])
+#     dd = dd[inverse_index].view(28, 28) 
+#     plt.imshow(dd)
+#     plt.subplot(2,3,3)
+#     d = datasets[2]['data'][n][0]
+#     plt.imshow(d)
+#     plt.subplot(2,3,6)
+#     dd = d.view(-1)
+#     inverse_index = np.argsort(datasets[2]['index'])
+#     dd = dd[inverse_index].view(28, 28)
+#     plt.imshow(dd)
+#     plt.show
---
+++ Experiments/Experiments6_ToyExperiments/experiment2.py
@@ -0,0 +1,344 @@
+# -*- encoding: utf-8 -*-
+'''
+@Time        :2020/07/19 16:16:39
+@Author      :Qu Yue
+@File        :main_cnn.py
+@Software    :Visual Studio Code
+Introduction:  
+'''
+#%% Import Packages
+# %matplotlib qt5
+import torch
+import torchvision
+import torch.utils.data as Data
+import time 
+import numpy as np
+import matplotlib.pyplot as plt
+import copy
+import multiprocessing
+from data_input import data_input, data_split, DATASET
+import sys
+sys.path.append('../..') # add the path which includes the packages
+from FusionLearning.Plugin import Plugin
+from FusionLearning import Fusion
+from model import *
+from process import *
+from drawing import draw_result
+
+#%% Hyper Parameters
+class PARM:
+    def __init__(self):
+        self.data = DATASET() 
+        self.dataset_ID = 2
+        self.test_size = 0.2
+        self.epoch = 100
+        self.batch_size = 10000
+        self.lr = 0.1
+        self.draw = True
+        self.cuda = True
+        self.showepoch = 1
+        self.random_seed = 1
+        self.fusion_lr = 1e-12 # 0.000000000001
+        self.Lambda = 0
+        self.model =  FNN1
+        self.time = dict()
+    @property
+    def dataset_name(self):
+        return self.data.data_dict[self.dataset_ID]
+    @property
+    def task_number(self):
+        return self.data.tasks[self.dataset_name] 
+
+    def lambda_list(self, ifzero=False):
+        if self.model == FNN1:
+            if ifzero == False:
+                lambda_list = [[0.48, 0.52],[0.33, 0.5, 0.5, 0.4, 0.57],[0.5, 0.48, 0.6]]
+            else:
+                lambda_list = [[0.5, 0.6],[0.35, 0.5, 0.5, 0.4, 0.6],[0.51, 0.5, 0.51]]
+
+        elif self.model == CNN1:
+            if ifzero == False:
+                lambda_list = [[0.54, 0.5],[0.57, 0.50, 0.5, 0.5, 0.51],[0.5,0.5,0.5]]
+            else:
+                lambda_list = [[0.5, 0.58],[0.62,0.5,0.5,0.7,0.5],[0.5,0.5,0.5]]
+        return lambda_list[self.dataset_ID-1]
+
+
+Parm = PARM()
+#%%
+if Parm.cuda and torch.cuda.is_available():
+    print('Using GPU.')
+else:
+    Parm.cuda = False
+    print('Using CPU.')
+
+torch.manual_seed(Parm.random_seed)
+torch.cuda.manual_seed(Parm.random_seed)
+
+#%% Task
+class TASK:
+    def __init__(self, ID=0):
+        self.ID = ID
+        self.train = []
+        self.test = []
+        self.train_accuracy = {ID:[]}
+        self.test_accuracy = {ID:[]}
+
+#%% Tasks data
+datasets = data_input(Parm)
+Tasks = []
+Train = [[],[]]
+Test = [[],[]]
+Origin = TASK('origin')
+Fusion_task = TASK('fusion')
+for i in range(Parm.task_number):
+    task = TASK(i)
+    data = data_split(datasets[i]['data'], datasets[i]['target'], 
+                      Parm.test_size, random_state=Parm.random_seed)
+    task.train = Data.TensorDataset(torch.stack(data[0][0]).type(torch.FloatTensor), 
+                                    torch.tensor(data[0][1]).type(torch.LongTensor))
+    Train[0].extend(data[0][0])
+    Train[1].extend(data[0][1])
+    Test[0].extend(data[1][0])
+    Test[1].extend(data[1][1])
+    task.test = Data.TensorDataset(torch.stack(data[1][0]).type(torch.FloatTensor), 
+                                    torch.tensor(data[1][1]).type(torch.LongTensor))
+    task.train_loader = Data.DataLoader(dataset=task.train,
+                                        batch_size=Parm.batch_size,
+                                        shuffle=True)
+    task.test_loader = Data.DataLoader(dataset=task.test, 
+                                       batch_size=1000,
+                                       shuffle=False)
+    Tasks.append(task)
+
+Origin.train = Data.TensorDataset(torch.stack(Train[0]).type(torch.FloatTensor),
+                            torch.tensor(Train[1]).type(torch.LongTensor))
+Origin.test = Data.TensorDataset(torch.stack(Test[0]).type(torch.FloatTensor), 
+                            torch.tensor(Test[1]).type(torch.LongTensor))
+Origin.train_loader = Data.DataLoader(dataset=Origin.train,
+                                      batch_size=Parm.batch_size,
+                                      shuffle=True)
+Origin.test_loader = Data.DataLoader(dataset=Origin.test, 
+                                     batch_size=1000,
+                                     shuffle=False)
+Fusion_task.train, Fusion_task.test, Fusion_task.train_loader, Fusion_task.test_loader = Origin.train, Origin.test, Origin.train_loader, Origin.test_loader
+#%% Create Models
+Model = Parm.model
+Origin.model =  Model() if Parm.cuda==False else Model().cuda()
+Origin.optimizer = torch.optim.SGD(Origin.model.parameters(), lr=Parm.lr)
+for i in range(Parm.task_number):
+    Tasks[i].model = Model() if Parm.cuda==False else Model().cuda()
+    Tasks[i].optimizer = torch.optim.SGD(Tasks[i].model.parameters(), lr=Parm.lr)
+fusion_model = Model() if Parm.cuda==False else Model().cuda()
+
+loss_func = torch.nn.CrossEntropyLoss()
+
+#%% Train
+if Parm.draw:
+    fig = plt.figure(1)
+    plt.ion()
+start = time.time()
+for epoch in range(Parm.epoch):
+    for task in Tasks:
+        training_process(task, loss_func, Parm)
+        testing_process(task, Parm)
+    if Parm.draw:
+        accuracy, name = [], []
+        for task in Tasks:
+            accuracy.append(task.test_accuracy[task.ID])
+            name.append(f"Task{task.ID}")
+        draw_result(accuracy, fig, name, True)
+finish = time.time()
+Parm.time['solo_train'] = finish - start
+
+#%% Adding Plugin
+print('Before')
+for i in range(Parm.task_number):
+    Tasks[i].model0 = Tasks[i].model
+    Tasks[i].model = copy.deepcopy(Plugin(Tasks[i].model))
+    Tasks[i].model.plugin_hook()
+    print(f"Accuracy: {testing_free(Tasks[i], Tasks[i].test_loader, Parm)}")
+    print(f"Total Accuracy: {testing_free(Tasks[i], Origin.test_loader, Parm)}")
+fusion_model = Plugin(fusion_model)
+
+#%% Average
+print('Average Fusion')
+
+for i in range(Parm.task_number):
+    Tasks[i].model = copy.deepcopy(Plugin(Tasks[i].model0))
+    Tasks[i].model.plugin_hook()
+models = [Task.model for Task in Tasks]
+start = time.time()
+fusion_model = Fusion.average_fusion(models, fusion_model)
+finish = time.time()
+Parm.time['AverFusion'] = finish - start
+for i in range(Parm.task_number):
+    print(f"Accuracy: {testing_free(fusion_model, Tasks[i].test_loader, Parm)}")
+print(f"Total Accuracy: {testing_free(fusion_model, Origin.test_loader, Parm)}")
+torch.cuda.empty_cache()
+
+#%%
+print('Pinv Fusion')
+for i in range(Parm.task_number):
+    Tasks[i].model = copy.deepcopy(Plugin(Tasks[i].model0, False))
+    Tasks[i].model.plugin_hook()
+start = time.time()
+fusion_model = Fusion.pinv_fusion_batch(Tasks, fusion_model, Parm, ifbatch=True)
+end = time.time()
+Parm.time['PinvFusion'] = finish - start
+for i in range(Parm.task_number):
+    print(f"Accuracy: {testing_free(fusion_model, Tasks[i].test_loader, Parm)}")
+print(f"Total Accuracy: {testing_free(fusion_model, Origin.test_loader, Parm)}")
+torch.cuda.empty_cache()
+
+#%%
+print('Pinv Fusion Weight')
+for i in range(Parm.task_number):
+    Tasks[i].model = copy.deepcopy(Plugin(Tasks[i].model0))
+    Tasks[i].model.plugin_hook()
+start = time.time()
+fusion_model = Fusion.pinv_fusion_batch(Tasks, fusion_model, Parm, ifbatch=True, ifweight=True)
+end = time.time()
+Parm.time['PinvFusionw'] = finish - start
+for i in range(Parm.task_number):
+    print(f"Accuracy: {testing_free(fusion_model, Tasks[i].test_loader, Parm)}")
+print(f"Total Accuracy: {testing_free(fusion_model, Origin.test_loader, Parm)}")
+torch.cuda.empty_cache()
+#%%
+print('Pinv Fusion lambda')
+for i in range(Parm.task_number):
+    Tasks[i].model = copy.deepcopy(Plugin(Tasks[i].model0, True))
+    Tasks[i].model.plugin_hook()
+start = time.time()
+fusion_model = Fusion.pinv_fusion_batch(Tasks, fusion_model, Parm, ifbatch=True, ifweight=True, lambda_list=Parm.lambda_list())
+end = time.time()
+Parm.time['PinvFusionl'] = finish - start
+for i in range(Parm.task_number):
+    print(f"Accuracy: {testing_free(fusion_model, Tasks[i].test_loader, Parm)}")
+print(f"Total Accuracy: {testing_free(fusion_model, Origin.test_loader, Parm)}")
+torch.cuda.empty_cache()
+
+#%%
+print('Pinv Fusion Zero')
+for i in range(Parm.task_number):
+    Tasks[i].model = copy.deepcopy(Plugin(Tasks[i].model0, True))
+    Tasks[i].model.plugin_hook()
+start = time.time()
+Tasks = Fusion.zero_rank(Tasks, Parm)
+fusion_model = Fusion.pinv_fusion_batch(Tasks, fusion_model, Parm, ifbatch=True)
+end = time.time()
+Parm.time['PinvFusion_z'] = finish - start
+for i in range(Parm.task_number):
+    print(f"Accuracy: {testing_free(fusion_model, Tasks[i].test_loader, Parm)}")
+print(f"Total Accuracy: {testing_free(fusion_model, Origin.test_loader, Parm)}")
+torch.cuda.empty_cache()
+
+#%%
+print('Pinv Fusion Zero Weight')
+for i in range(Parm.task_number):
+    Tasks[i].model = copy.deepcopy(Plugin(Tasks[i].model0, True))
+    Tasks[i].model.plugin_hook()
+start = time.time()
+Tasks = Fusion.zero_rank(Tasks, Parm)
+fusion_model = Fusion.pinv_fusion_batch(Tasks, fusion_model, Parm, ifbatch=True, ifweight=True)
+end = time.time()
+Parm.time['PinvFusionw_z'] = finish - start
+for i in range(Parm.task_number):
+    print(f"Accuracy: {testing_free(fusion_model, Tasks[i].test_loader, Parm)}")
+print(f"Total Accuracy: {testing_free(fusion_model, Origin.test_loader, Parm)}")
+torch.cuda.empty_cache()
+
+#%%
+print('Pinv Fusion Zero lambda')
+for i in range(Parm.task_number):
+    Tasks[i].model = copy.deepcopy(Plugin(Tasks[i].model0, True))
+    Tasks[i].model.plugin_hook()
+start = time.time()
+Tasks = Fusion.zero_rank(Tasks, Parm)
+fusion_model = Fusion.pinv_fusion_batch(Tasks, fusion_model, Parm, ifbatch=True, ifweight=True, lambda_list=Parm.lambda_list(True))
+end = time.time()
+Parm.time['PinvFusionl_z'] = finish - start
+for i in range(Parm.task_number):
+    print(f"Accuracy: {testing_free(fusion_model, Tasks[i].test_loader, Parm)}")
+print(f"Total Accuracy: {testing_free(fusion_model, Origin.test_loader, Parm)}")
+torch.cuda.empty_cache()
+
+
+#%%
+# print('MAS Fusion')
+# for i in range(Parm.task_number):
+#     Tasks[i].model = copy.deepcopy(Plugin(Tasks[i].model0))
+#     Tasks[i].model.plugin_hook()
+# fusion_model = Fusion.pinv_fusion(Tasks, fusion_model, Parm)
+# Parm.Lambda = 0.000001
+# for i in range(Parm.task_number):
+#     print(f"Accuracy: {testing_free(fusion_model, Tasks[i].test_loader, Parm)}") 
+# print(f"Total:{testing_free(fusion_model, Test_loader, Parm) :.5f}") 
+# # Parm.fusion_lr2 = [1e-14, 1e-11,1e-13]
+# Parm.fusion_lr2 = [1e-3, 1e-3,1e-3,1e-3, 1e-3,1e-3]
+# fusion_model, save1 = Fusion.MAS_fusion(Tasks, fusion_model, Parm, testing_free, True, Test_loader)
+
+#%%
+Origin.optimizer = torch.optim.SGD(Origin.model.parameters(), lr=0.1)
+if Parm.draw:
+    fig = plt.figure(2)
+    plt.ion()
+start = time.time()
+for epoch in range(Parm.epoch):
+    training_process(Origin, loss_func, Parm)
+    testing_process(Origin, Parm)
+    if Parm.draw:
+        accuracy, name = [], []
+        accuracy.append(Origin.test_accuracy[Origin.ID])
+        name.append(f"Task{Origin.ID}")
+        draw_result(accuracy, fig, name, True)
+finish = time.time()
+Parm.time['origin_train'] = finish - start
+#%%
+if Parm.draw:
+    fig = plt.figure(3)
+    plt.ion()
+print('Pinv Fusion')
+for i in range(Parm.task_number):
+    Tasks[i].model = copy.deepcopy(Plugin(Tasks[i].model0, False))
+    Tasks[i].model.plugin_hook()
+start = time.time()
+Tasks = Fusion.zero_rank(Tasks, Parm)
+fusion_model = Fusion.pinv_fusion_batch(Tasks, fusion_model, Parm, ifbatch=True, ifweight=False)
+
+end = time.time()
+Parm.time['PinvFusion'] = finish - start
+for i in range(Parm.task_number):
+    print(f"Accuracy: {testing_free(fusion_model, Tasks[i].test_loader, Parm)}")
+print(f"Total Accuracy: {testing_free(fusion_model, Origin.test_loader, Parm)}")
+torch.cuda.empty_cache()
+Fusion_task.model =  fusion_model
+Fusion_task.optimizer = torch.optim.SGD(Fusion_task.model.model.parameters(), lr=0.1)
+#%%**
+# Fusion_task.optimizer = torch.optim.SGD(Fusion_task.model.model.parameters(), lr=0.1)
+testing_process(Fusion_task, Parm)
+if Parm.draw:
+    fig = plt.figure(3)
+    plt.ion()
+start = time.time()
+for epoch in range(Parm.epoch):
+    training_process(Fusion_task, loss_func, Parm)
+    testing_process(Fusion_task, Parm)
+    if Parm.draw:
+        accuracy, name = [], []
+        accuracy.append(Fusion_task.test_accuracy[Fusion_task.ID])
+        name.append(f"Task{Fusion_task.ID}")
+        draw_result(accuracy, fig, name, True)
+finish = time.time()
+Parm.time['Fusion_task_train'] = finish - start
+
+
+# %%
+if Parm.draw:
+    plt.ioff()
+    plt.show()
+
+
+#%%
+plt.plot(Origin.test_accuracy['origin'])
+plt.plot([Fusion_task.test_accuracy['fusion'][0]]*20+Fusion_task.test_accuracy['fusion'])
